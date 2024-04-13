7012ac330ba2751a1c03fa7153202be832f95785
tradestealer999 <112725984+truerep@users.noreply.github.com>
2024-04-13 19:35:11 +0530

    Updated new portfolio

---
+++ script.js
@@ -0,0 +1,28 @@
+let lastScrollPos = 0;
+window.addEventListener('scroll', function() {
+  const currentScrollPos = window.scrollY;
+  const header = document.querySelector('.header');
+
+  if (currentScrollPos > lastScrollPos) {
+    header.classList.add('hide');
+  } else {
+    header.classList.remove('hide');
+  }
+
+  if (currentScrollPos <= 40) {
+    header.classList.remove('hide');
+  }
+
+  if (currentScrollPos >= 100) {
+    header.classList.add('bg-white');
+  } else {
+    header.classList.remove('bg-white');
+  }
+
+  lastScrollPos = currentScrollPos;
+});
+
+function toggleSidebar() {
+  document.querySelector('.hamburger-icon').classList.toggle('toggled');
+  document.querySelector('.nav').classList.toggle('active');
+}
